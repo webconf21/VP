b563e20ff785bf23270c4f498ddef9264b0a8edd
webconf21 <johnsmith2webconf21@gmail.com>
2020-12-02 23:22:17 +0530

    Added obs, running instructions for video preprocessing, contribution

--- Prediction/main.py
+++ Prediction/main.py
@@ -6,7 +6,7 @@ import pickle
 import random
 import warnings
 import time
-from pa import build_model
+from parima import build_model
 from bitrate import alloc_bitrate
 from qoe import calc_qoe
 
@@ -54,36 +54,83 @@ def get_data(data, frame_nos, dataset, topic, usernum):
 			n_objects.append(0)
 	total_objects=max(n_objects)
 
-	max_frame = int(view_info[-1][0]*1.0*fps/milisec)
+	if dataset == 1:
+		max_frame = int(view_info[-1][0]*1.0*fps/milisec)
 
-	for i in range(len(view_info)-1):
-		frame = int(view_info[i][0]*1.0*fps/milisec)
-		frame += int(offset*1.0*fps/milisec)
+		for i in range(len(view_info)-1):
+			frame = int(view_info[i][0]*1.0*fps/milisec)
+			frame += int(offset*1.0*fps/milisec)
 
-		frame_nos.append(frame)
-		if(frame > max_frame):
-			break
-		X={}
-		X['VIEWPORT_x']=int(view_info[i][1][1]*width/view_width)
-		X['VIEWPORT_y']=int(view_info[i][1][0]*height/view_height)
-		for j in range(total_objects):
-			try:
-				centroid = obj_info[frame][j]
+			frame_nos.append(frame)
+			if(frame > max_frame):
+				break
+			X={}
+			X['VIEWPORT_x']=int(view_info[i][1][1]*width/view_width)
+			X['VIEWPORT_y']=int(view_info[i][1][0]*height/view_height)
+			for j in range(total_objects):
+				try:
+					centroid = obj_info[frame][j]
 
-				if obj_info[frame][j] == None:
+					if obj_info[frame][j] == None:
+						X['OBJ_'+str(j)+'_x']=np.random.normal(0,1)
+						X['OBJ_'+str(j)+'_y']=np.random.normal(0,1)
+					else:
+						X['OBJ_'+str(j)+'_x']=centroid[0]
+						X['OBJ_'+str(j)+'_y']=centroid[1]
+
+				except:
 					X['OBJ_'+str(j)+'_x']=np.random.normal(0,1)
 					X['OBJ_'+str(j)+'_y']=np.random.normal(0,1)
-				else:
-					X['OBJ_'+str(j)+'_x']=centroid[0]
-					X['OBJ_'+str(j)+'_y']=centroid[1]
-
-			except:
-				X['OBJ_'+str(j)+'_x']=np.random.normal(0,1)
-				X['OBJ_'+str(j)+'_y']=np.random.normal(0,1)
 
 
-		data.append((X, int(view_info[i+1][1][1]*width/view_width),int(view_info[i+1][1][0]*height/view_height)))
-	
+			data.append((X, int(view_info[i+1][1][1]*width/view_width),int(view_info[i+1][1][0]*height/view_height)))
+
+	elif dataset == 2:
+		for k in range(len(view_info)-1):
+			if view_info[k][0]<=offset+60 and view_info[k+1][0]>offset+60:
+				max_frame = int(view_info[k][0]*1.0*fps/milisec)
+				break
+		
+		for k in range(len(view_info)-1):
+			if view_info[k][0]<=offset and view_info[k+1][0]>offset:
+				min_index = k+1
+				break
+				
+
+		prev_frame = 0
+		for i in range(min_index,len(view_info)-1):
+			frame = int((view_info[i][0])*1.0*fps/milisec)
+
+			if frame == prev_frame:
+				continue
+			
+			if(frame > max_frame):
+				break
+
+			frame_nos.append(frame)
+			
+			X={}
+			X['VIEWPORT_x']=int(view_info[i][1][1]*width/view_width)
+			X['VIEWPORT_y']=int(view_info[i][1][0]*height/view_height)
+			for j in range(total_objects):
+				try:
+					centroid = obj_info[frame][j]
+
+					if obj_info[frame][j] == None:
+						X['OBJ_'+str(j)+'_x']=np.random.normal(0,1)
+						X['OBJ_'+str(j)+'_y']=np.random.normal(0,1)
+					else:
+						X['OBJ_'+str(j)+'_x']=centroid[0]
+						X['OBJ_'+str(j)+'_y']=centroid[1]
+
+				except:
+					X['OBJ_'+str(j)+'_x']=np.random.normal(0,1)
+					X['OBJ_'+str(j)+'_y']=np.random.normal(0,1)
+
+
+			data.append((X, int(view_info[i+1][1][1]*width/view_width),int(view_info[i+1][1][0]*height/view_height)))
+			prev_frame = frame
+			
 	return data, frame_nos, max_frame,total_objects
 
 
